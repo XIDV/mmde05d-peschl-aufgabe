ae011efc1ad8ee10a3b932f6ac5ba566f02d07c0
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-09-24 12:32:32 +0200

    Mouseover-Animation neu implementiert (v2)

--- src/main.js
+++ src/main.js
@@ -1,25 +1,78 @@
 document.addEventListener('DOMContentLoaded', dclEvent => {
     console.log('Ready');
 
-    // Ausgabe von Begüßung mit aktuellem Datum
+
+    
+    // Ausgabe von Begüßung mit aktuellem Datum +++++++++++++++++++++++++++++ +
+    
     document.getElementById('greetingContainer').textContent = getFullGreetingText();
 
-    // Mouseover-Effekt f. Demo-Bilder
+    
+    // Mouseover-Effekt f. Demo-Bilder ++++++++++++++++++++++++++++++++++++++ +
+
+    // Variante 1 ----------------------------------------------------------- +
+    // let demoImgs = document.getElementsByClassName('demoImg');
+    // for(img of demoImgs) {
+    //     img.addEventListener('mouseover', e => {
+    //         e.target.style.borderRadius = '1rem';
+    //         e.target.style.transform = 'scale(1.05)';
+    //         e.target.style.opacity = '0';
+    //         setTimeout(() => {
+    //             e.target.src = `src/img/${e.target.name}_blur.jpg`;
+    //         }, 250);
+    //         setTimeout(() => {
+    //             e.target.style.opacity = '1';
+    //         }, 250);
+    //     });
+
+    //     img.addEventListener('mouseleave', e => {
+    //         e.target.style.borderRadius = '0';
+    //         e.target.style.transform = 'scale(1)';
+    //         setTimeout(() => {
+    //             e.target.src = `src/img/${e.target.name}.jpg`;
+    //         }, 250);
+    //         setTimeout(() => {
+    //             e.target.style.opacity = '1';
+    //         }, 250);
+    //     });
+    // }
+    
+    // Variante 2 ----------------------------------------------------------- +
     let demoImgs = document.getElementsByClassName('demoImg');
     for(img of demoImgs) {
         img.addEventListener('mouseover', e => {
-            e.target.style.borderRadius = '1rem';
-            e.target.style.transform = 'scale(1.05)';
-            e.target.src = `src/img/${e.target.name}_blur.jpg`;
+            if(document.getElementById('overlay')) {
+                document.getElementById('overlay').remove();
+            }
+            let overlay = document.createElement('img');
+            let imgParent = e.target.parentElement;
+            overlay.setAttribute('id', 'overlay');
+            overlay.setAttribute('src',`src/img/${e.target.name}_blur.jpg`);
+            imgParent.appendChild(overlay);
+            e.target.style.opacity = '0';
+            setTimeout(() => {
+                overlay.style.opacity = '1';
+                overlay.style.borderRadius = '1.5rem';
+                imgParent.style.transform = 'scale(1.05)';
+            }, 50);
         });
 
-        img.addEventListener('mouseleave', e => {
-            e.target.style.borderRadius = '0';
-            e.target.style.transform = 'scale(1)';
-            e.target.src = `src/img/${e.target.name}.jpg`;
+        img.addEventListener('mouseout', e => {
+            let overlay = document.getElementById('overlay');
+            let imgParent = e.target.parentElement;
+            e.target.style.opacity = '1';
+            overlay.style.opacity = '0';
+            overlay.style.borderRadius = '0';
+            imgParent.style.transform = 'scale(1)';
+            setTimeout(() => {
+                overlay.remove();
+            }, 250);
         });
     }
+    
 });
+
+
 
 
 /* Erstelle auf Basis eines Date-Objektes einen String bestehend aus Begrüßung und Datum.
