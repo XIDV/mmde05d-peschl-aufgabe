b160c95a3db41873f08f5763bd8fd109225c1b85
Sebastian@XIDV <sebastian.peschl@xidv.de>
2022-09-25 12:21:42 +0200

    Auswahl u. Funktion d. Bildwechsel-Varianten fertiggestellt.

--- src/main.js
+++ src/main.js
@@ -18,110 +18,39 @@ document.addEventListener('DOMContentLoaded', dclEvent => {
     for(imgChangeSelctor of imgChangeSelectors) {
         imgChangeSelctor.addEventListener('change', e => {
             imgChangeSelection = parseInt(e.target.value);
-        });
-    }
-
-    
-    
-    // Mouseover-Effekt f. Demo-Bilder ++++++++++++++++++++++++++++++++++++++ +
-
-    // Variante 1 ----------------------------------------------------------- +
-    // let demoImgs = document.getElementsByClassName('demoImg');
-    // for(img of demoImgs) {
-    //     img.addEventListener('mouseover', e => {
-    //         e.target.style.borderRadius = '1rem';
-    //         e.target.style.transform = 'scale(1.05)';
-    //         e.target.style.opacity = '0';
-    //         setTimeout(() => {
-    //             e.target.src = `src/img/${e.target.name}_blur.jpg`;
-    //         }, 250);
-    //         setTimeout(() => {
-    //             e.target.style.opacity = '1';
-    //         }, 250);
-    //     });
-
-    //     img.addEventListener('mouseleave', e => {
-    //         e.target.style.borderRadius = '0';
-    //         e.target.style.transform = 'scale(1)';
-    //         setTimeout(() => {
-    //             e.target.src = `src/img/${e.target.name}.jpg`;
-    //         }, 250);
-    //         setTimeout(() => {
-    //             e.target.style.opacity = '1';
-    //         }, 250);
-    //     });
-    // }
-    
-    // Variante 2 ----------------------------------------------------------- +
-    /*
-        (1) Erfasse alle Elemente mit der Klasse "demoImg" und sichere diese
-        Elmente in die Element-Liste "demoImgs"
-        (2) Für jedes Element mit der Klasse "demoImg" registriere Event-
-        Listener für "mouseover" und "mouseout".
-    */
-    let demoImgs = document.getElementsByClassName('demoImg');
-    for(img of demoImgs) {
-        /*
-            Wenn sich der Mauszeiger innerhalb des Elements befindet dann ...
-            (a) prüfe ob sich im DOM bereits ein Element mit der ID "oberlay"
-            befindet. Wenn das der Fall ist, dann entferne dieses Element.
-            (b) Erstelle ein neues Element vom Typ "img".
-            (c) Sichere das Eltern-Element in der Variablen "imgParent"
-            (d) Setze die Attribute "id" und "src" für das neue "img"-Element.
-            (e) Füge dem "imgParent" das neue "img"-Element m. d. ID "overlay"
-            hinzu.
-            (f) Veringere die Sichtbarkeit des "target"-Elements auf 0.
-            (g) Warte 50ms und erhöhe dann die Sichtbarkeit von "overlay" auf 1
-            und setze den "borderRadius" auf 1.5rem. Erhöhe außerdem die
-            Skallierung des "imgParent" auf 1.05.
-        */
-        img.addEventListener('mouseover', e => {
-            if(document.getElementById('overlay')) {
-                document.getElementById('overlay').remove();
+            let demoImgs = document.getElementsByClassName('demoImg');
+            
+            if (imgChangeSelection != 1) {
+                if (imgChangeSelection == 2) {
+                    //Variante 2 -------------------------------------------- +
+                    removeAllImgChangeListener(demoImgs);
+                    for(img of demoImgs) {
+                        img.addEventListener('mouseover', changeImgOver);
+                        img.addEventListener('mouseout', changeImgOut);
+                    }
+                } else if (imgChangeSelection == 3) {
+                    // Variante 3 ------------------------------------------- +
+                    /*
+                        Für jedes Element mit der Klasse "demoImg" registriere Event-
+                        Listener für "mouseover" und "mouseout".
+                    */
+                        removeAllImgChangeListener(demoImgs);
+                    for(img of demoImgs) {
+                        img.addEventListener('mouseover', overlayOver);
+                        img.addEventListener('mouseout', overlayOut);
+                    }
+                }
+                
+            } else {
+                removeAllImgChangeListener(demoImgs);
             }
-            let overlay = document.createElement('img');
-            let imgParent = e.target.parentElement;
-            overlay.setAttribute('id', 'overlay');
-            overlay.setAttribute('src',
-            `src/img/${e.target.name}_blur.jpg`);
-            imgParent.appendChild(overlay);
-            e.target.style.opacity = '0';
-            setTimeout(() => {
-                overlay.style.opacity = '1';
-                overlay.style.borderRadius = '1.5rem';
-                imgParent.style.transform = 'scale(1.05)';
-            }, 50);
         });
-
-        /*
-            Wenn sich der Mauszeiger außerhalb eines der erfassten Elementen
-            befindet dann ...
-            (a) Erfasse das Element mit der ID "overlay" in der Variablen
-            "overlay".
-            (b) Erfasse das Eltern-Element des target-Elements.
-            (c) Setze die Sichtbarkeit des target-Elements auf 1
-            (d) Setze Sichtbarkeit und "borderRadius" von "oberlay" auf 0
-            (e) Setze die Skalierung des "imgParent" wieder auf 1 zurück.
-            (f) Warte 250ms und entferne "overlay" dann aus dem DOM
-        */
-        img.addEventListener('mouseout', e => {
-            let overlay = document.getElementById('overlay');
-            let imgParent = e.target.parentElement;
-            e.target.style.opacity = '1';
-            overlay.style.opacity = '0';
-            overlay.style.borderRadius = '0';
-            imgParent.style.transform = 'scale(1)';
-            setTimeout(() => {
-                overlay.remove();
-            }, 250);
-        });
-    }
+    } 
     
 });
 
 
-
-
+// Funktionen f. Datumausgabe #################################################
 /*
     Erstelle auf Basis eines Date-Objektes einen String bestehend aus Begrüßung
     und Datum. Gebe diesen String zurück.
@@ -143,4 +72,112 @@ function getGreeting(hour) {
     } else {
         return 'Guten Abend.';
     }
+}
+
+
+// Funktionen f. Bildwechsel ##################################################
+/* 
+    Entferne alle existierenden Event-Listener bzgl. "mouseover" u. "mouseout" die den 
+    Elementen in "demoImgs" zugeordnet sind.
+*/
+function removeAllImgChangeListener(demoImgs) {
+    for(img of demoImgs) {
+        img.removeEventListener('mouseover', overlayOver);
+        img.removeEventListener('mouseover', changeImgOver);
+        img.removeEventListener('mouseout', overlayOut);
+        img.removeEventListener('mouseout', changeImgOut);
+    }
+}
+
+
+
+// Funktionen f. Bildwechsel-Variante 1 +++++++++++++++++++++++++++++++++++++ +
+function overImgHandler(element) {
+    element.src = `src/img/${element.name}_blur.jpg`;
+}
+
+function outImgHandler(element) {
+    element.src = `src/img/${element.name}.jpg`;
+}
+
+
+// Funktionen für Bildwechsel-Variante 2 ++++++++++++++++++++++++++++++++++++ +
+
+function changeImgOver(e) {
+    e.target.style.borderRadius = '1rem';
+    e.target.style.transform = 'scale(1.05)';
+    e.target.style.opacity = '0';
+    setTimeout(() => {
+        e.target.src = `src/img/${e.target.name}_blur.jpg`;
+    }, 250);
+    setTimeout(() => {
+        e.target.style.opacity = '1';
+    }, 250);
+}
+
+function changeImgOut(e) {
+    e.target.style.borderRadius = '0';
+    e.target.style.transform = 'scale(1)';
+    setTimeout(() => {
+        e.target.src = `src/img/${e.target.name}.jpg`;
+    }, 250);
+    setTimeout(() => {
+        e.target.style.opacity = '1';
+    }, 250);
+}
+
+// Funktionen für Bildwechsel-Variante 3 ++++++++++++++++++++++++++++++++++++ +
+/*
+    Wenn sich der Mauszeiger innerhalb des Elements befindet dann ...
+    (a) prüfe ob sich im DOM bereits ein Element mit der ID "oberlay"
+    befindet. Wenn das der Fall ist, dann entferne dieses Element.
+    (b) Erstelle ein neues Element vom Typ "img".
+    (c) Sichere das Eltern-Element in der Variablen "imgParent"
+    (d) Setze die Attribute "id" und "src" für das neue "img"-Element.
+    (e) Füge dem "imgParent" das neue "img"-Element m. d. ID "overlay"
+    hinzu.
+    (f) Veringere die Sichtbarkeit des "target"-Elements auf 0.
+    (g) Warte 50ms und erhöhe dann die Sichtbarkeit von "overlay" auf 1
+    und setze den "borderRadius" auf 1.5rem. Erhöhe außerdem die
+    Skallierung des "imgParent" auf 1.05.
+*/
+function overlayOver(e) {
+    if(document.getElementById('overlay')) {
+        document.getElementById('overlay').remove();
+    }
+    let overlay = document.createElement('img');
+    let imgParent = e.target.parentElement;
+    overlay.setAttribute('id', 'overlay');
+    overlay.setAttribute('src',
+    `src/img/${e.target.name}_blur.jpg`);
+    imgParent.appendChild(overlay);
+    e.target.style.opacity = '0';
+    setTimeout(() => {
+        overlay.style.opacity = '1';
+        overlay.style.borderRadius = '1.5rem';
+        imgParent.style.transform = 'scale(1.05)';
+    }, 50);
+}
+
+/*
+    Wenn sich der Mauszeiger außerhalb eines der erfassten Elementen
+    befindet dann ...
+    (a) Erfasse das Element mit der ID "overlay" in der Variablen
+    "overlay".
+    (b) Erfasse das Eltern-Element des target-Elements.
+    (c) Setze die Sichtbarkeit des target-Elements auf 1
+    (d) Setze Sichtbarkeit und "borderRadius" von "oberlay" auf 0
+    (e) Setze die Skalierung des "imgParent" wieder auf 1 zurück.
+    (f) Warte 250ms und entferne "overlay" dann aus dem DOM
+*/
+function overlayOut(e) {
+    let overlay = document.getElementById('overlay');
+    let imgParent = e.target.parentElement;
+    e.target.style.opacity = '1';
+    overlay.style.opacity = '0';
+    overlay.style.borderRadius = '0';
+    imgParent.style.transform = 'scale(1)';
+    setTimeout(() => {
+        overlay.remove();
+    }, 250);
 }
